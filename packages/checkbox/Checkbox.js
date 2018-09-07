ef74326f9cc2f38d3ae06b38181ed1c760a065e6
OhNiiice <jay@betazuul.com>
2018-09-07 17:59:10 -0500

    updated checkbox icon button

--- packages/checkbox/Checkbox.js
+++ packages/checkbox/Checkbox.js
@@ -12,13 +12,21 @@ class Checkbox extends React.Component {
     this.checkboxEl = null;
     this.nativeCheckboxEl = null;
     this.state = {
-      classList: new Set()
+      classList: new Set(),
     };
   }
 
   componentDidMount() {
-    const { checked, disabled, indeterminate, value } = this.props;
-    
+    const {
+      checked, disabled, indeterminate, value,
+    } = this.props;
+
+    this.checkboxEl.addEventListener(
+      getCorrectEventName(window, 'animationend'),
+      this.handleAnimationEnd,
+    );
+    this.nativeCheckboxEl.addEventListener('change', this.handleChange);
+
     this.foundation = new MDCCheckboxFoundation(this.adapter);
     this.foundation.init();
     this.foundation.setChecked(checked);
@@ -33,35 +41,17 @@ class Checkbox extends React.Component {
 
   get adapter() {
     const { classList } = this.state;
-
     return {
-      addClass: className =>
-        this.setState({ classList: classList.add(className) }),
-      removeClass: className => {
+      addClass: className => this.setState({ classList: classList.add(className) }),
+      removeClass: (className) => {
         classList.delete(className);
         this.setState({ classList });
       },
-      setNativeControlAttr: (attr, value) =>
-        this.nativeCheckboxEl.setAttribute(attr, value),
-      removeNativeControlAttr: attr =>
-        this.nativeCheckboxEl.removeAttribute(attr),
-      registerAnimationEndHandler: handler =>
-        this.checkboxEl.addEventListener(
-          getCorrectEventName(window, 'animationend'),
-          handler
-        ),
-      deregisterAnimationEndHandler: handler =>
-        this.checkboxEl.removeEventListener(
-          getCorrectEventName(window, 'animationend'),
-          handler
-        ),
-      registerChangeHandler: handler =>
-        this.nativeCheckboxEl.addEventListener('change', handler),
-      deregisterChangeHandler: handler =>
-        this.nativeCheckboxEl.removeEventListener('change', handler),
+      setNativeControlAttr: (attr, value) => this.nativeCheckboxEl.setAttribute(attr, value),
+      removeNativeControlAttr: attr => this.nativeCheckboxEl.removeAttribute(attr),
       getNativeControl: () => this.nativeCheckboxEl,
       forceLayout: () => this.checkboxEl.offsetWidth,
-      isAttachedToDOM: () => Boolean(this.checkboxEl.parentNode)
+      isAttachedToDOM: () => Boolean(this.checkboxEl.parentNode),
     };
   }
 
@@ -72,40 +62,96 @@ class Checkbox extends React.Component {
     return classnames('mdc-checkbox', Array.from(classList), className);
   }
 
-  handleLabelClick = e => {
+  handleChange = () => {
+    this.foundation.handleChange();
+  };
+
+  handleAnimationEnd = () => {
+    this.foundation.handleAnimationEnd();
+  };
+
+  handleLabelClick = (e) => {
     if (this.foundation.isDisabled()) return;
-    
+
     const { onMouseDown, onMouseUp } = this.props;
-    
+
     onMouseDown(e);
     setTimeout(() => {
       onMouseUp(e);
     }, 100);
-    
+
     this.foundation.setChecked(!this.foundation.isChecked());
     if (this.foundation.isIndeterminate()) {
       this.foundation.setIndeterminate(false);
     }
   };
 
-  initCheckbox = instance => {
+  initCheckbox = (instance) => {
     const { initRipple } = this.props;
 
     initRipple(instance);
     this.checkboxEl = instance;
   };
 
-  initNativeCheckbox = instance => {
+  initNativeCheckbox = (instance) => {
     this.nativeCheckboxEl = instance;
   };
+
+  renderBackground = () => (
+    <div className="mdc-checkbox__background">
+      <svg className="mdc-checkbox__checkmark" viewBox="0 0 24 24">
+        <path
+          className="mdc-checkbox__checkmark-path"
+          fill="none"
+          d="M1.73,12.91 8.1,19.28 22.79,4.59"
+        />
+      </svg>
+      <div className="mdc-checkbox__mixedmark" />
+    </div>
+  );
+
+  renderInput() {
+    const { id, name } = this.props;
+    return (
+      <input
+        type="checkbox"
+        className="mdc-checkbox__native-control"
+        id={id}
+        name={name}
+        ref={this.initNativeCheckbox}
+      />
+    );
+  }
+
+  renderLabel() {
+    const { disabled, id, label } = this.props;
+    if (label) {
+      const classes = classnames('bmc-checkbox-label', {
+        'bmc-checkbox-label--disabled': disabled,
+      });
+      return (
+        <label className={classes} htmlFor={id} onClick={this.handleLabelClick}>
+          {label}
+        </label>
+      );
+    }
+
+    return null;
+  }
 
   render() {
     const {
       /* eslint-disable no-unused-vars */
+      checked,
       className,
+      disabled,
+      id,
       indeterminate,
       initRipple,
+      label,
+      name,
       unbounded,
+      value,
       /* eslint-enable no-unused-vars */
       ...otherProps
     } = this.props;
@@ -120,51 +166,6 @@ class Checkbox extends React.Component {
       </React.Fragment>
     );
   }
-
-  renderBackground() {
-    return (
-      <div className="mdc-checkbox__background">
-        <svg className="mdc-checkbox__checkmark" viewBox="0 0 24 24">
-          <path
-            className="mdc-checkbox__checkmark-path"
-            fill="none"
-            d="M1.73,12.91 8.1,19.28 22.79,4.59"
-          />
-        </svg>
-        <div className="mdc-checkbox__mixedmark" />
-      </div>
-    );
-  }
-
-  renderInput() {
-    const { id, name } = this.props;
-
-    return (
-      <input
-        type="checkbox"
-        className="mdc-checkbox__native-control"
-        id={id}
-        name={name}
-        ref={this.initNativeCheckbox}
-      />
-    );
-  }
-
-  renderLabel() {
-    const { disabled, id, label } = this.props;
-
-    return label ? (
-      <label
-        className={classnames('bmc-checkbox-label', {
-          'bmc-checkbox-label--disabled': disabled
-        })}
-        htmlFor={id}
-        onClick={this.handleLabelClick}
-      >
-        {label}
-      </label>
-    ) : null;
-  }
 }
 
 Checkbox.propTypes = {
@@ -176,19 +177,16 @@ Checkbox.propTypes = {
   label: PropTypes.string,
   name: PropTypes.string.isRequired,
   unbounded: PropTypes.bool,
-  value: PropTypes.string.isRequired
+  value: PropTypes.string.isRequired,
 };
 
 Checkbox.defaultProps = {
   checked: false,
   disabled: false,
-  id: null,
   indeterminate: false,
   initRipple: () => {},
   label: null,
-  name: null,
   unbounded: true,
-  value: null
 };
 
 export default withRipple(Checkbox);
